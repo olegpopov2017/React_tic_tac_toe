fc108c718c20e0e975b10ba064ab89aa8376daff
oleg <olegpopov2017@mail.ru>
2025-02-25 07:54:23 +0300

    Interactivity finished.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
-function Square({ value }) {
+import { useState } from 'react'
+
+function Square() {
+  const [value, setValue] = useState(null)
+
   function handleClick() {
-    console.log('the button is clicket')
+    setValue('x')
   }
 
   return (
@@ -14,20 +18,20 @@ export default function Board() {
   return (
     <>
       <div className="board-row">
-        <Square value="1" />
-        <Square value="2" />
-        <Square value="3" />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="4" />
-        <Square value="5" />
-        <Square value="6" />
+        <Square />
+        <Square />
+        <Square />
       </div>
 
       <div className="board-row">
-        <Square value="7" />
-        <Square value="8" />
-        <Square value="9" />
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   )
